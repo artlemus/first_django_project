1b0238e1506683917da9bbff352f0e96276bd91b
artlemus <artlemus91@gmail.com>
2020-05-13 22:44:27 -0700

    first commit

--- rental/views.py
+++ rental/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 # Create your views here.
 # MVC python manage.py runserver
 
 # defining an endpoint
 def index(request):
-    return HttpResponse('hello world')
+    all_movies = Movie.objects.all() #read the  movie table to list
+    return render(request,'index.html',{'title':'Movies Catalog', 'movies': all_movies })
 
 def about(request):
     return HttpResponse('Art Lemus')
     
 def soon(request):
-    return HttpResponse('Page coming soon, stay in touch :) ')
+    return render(request,'comingSoon.html')
